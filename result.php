35b8600b53f4ab6ecf99f13a84d988b60cc37b10
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-11-20 22:20:44 -0600

    test

--- result.php
+++ result.php
@@ -108,8 +108,11 @@ $result1 = $sns->listSubscriptions(array(
 
 print_r($result1);
 
-$snsendpoint = $result1['Subscriptions'][4]['TopicArn'];
+$snsendpoint = $result1['Subscriptions'][0]['SubscriptionArn'];
     echo "============\n". test======= $snsendpoint . "================";
+
+$snsendpoint1 = $result1['Subscriptions'][0]['SubscriptionArn']['TopicArn'];
+    echo "============\n". test======= $snsendpoint1 . "================";
 
 
 $link->real_query("SELECT * FROM MiniProject1");
